b2a6373302135a8b6a4e7341764d24666df4d21c
Alex Via <alexandervia@gmail.com>
2022-08-28 12:56:59 -0500

    Adds showPost method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,8 @@
       return {
         message: "Welcome to your app!",
         posts: [],
-        newPost: {}
+        newPost: {},
+        currentPost: {}
       };
     },
     created: function () {
@@ -23,6 +24,11 @@
           console.log(response.data);
           this.posts.push(response.data);
         })
+      },
+      showPost: function (thePost) {
+        console.log("sjowing post");
+        this.currentPost = thePost;
+        document.querySelector('#post-details').showModal();
       }
     },
   };
@@ -32,6 +38,7 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <hr/>
+    <!-- Create A Post -->
     <p><b>Image URL: </b><input type="text" v-model="newPost.image_url" /></p>
     <p><b>Description: </b><input type="text" v-model="newPost.description" /></p>
     <p><b>Latitude: </b><input type="text" v-model="newPost.latitude" /></p>
@@ -39,13 +46,24 @@
     <p><b>User ID: </b><input type="text" v-model="newPost.user_id" /></p>
     <button v-on:click="createPost()">Create Post</button>
     <hr/>
-    <div v-for="post in posts">
-    <p>{{ post.image_url }}</p>
+  <!-- Index Posts -->
+    <div v-for="post in posts" v-bind:key="post.id">
+    <img :src="post.image_url">
     <p>{{ post.description }}</p>
     <p><b>Latitude: </b>{{ post.latitude }}</p>
     <p><b>Longitude: </b>{{ post.longitude }}</p>
+    <button v-on:click="showPost(post)">More Info</button>
     <br/>
     <hr/>
+    <!-- Show Post (modal) -->
+    <dialog id="post-details">
+      <form method="dialog">
+        <img :src= currentPost.image_url />
+        <p><b>Latitude: </b>{{ currentPost.latitude }}</p>
+        <p><b>Longitude: </b>{{ currentPost.longitude }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
     </div>>
   </div>
 </template>
