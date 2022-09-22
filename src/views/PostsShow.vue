f3571f45bed1e3440c97851502f846cf52362079
Alex Via <alexandervia@gmail.com>
2022-09-22 16:16:38 -0500

    implements map display for updating post, not entirely functional yet

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -1,5 +1,6 @@
 <script>
-  import axios from 'axios'
+  import axios from 'axios';
+  import mapboxgl from 'mapbox-gl';
     export default {
       data: function () {
         return {
@@ -8,13 +9,16 @@
         };
       },
       created: function () {
-        this.postsShow()
+        this.postsShow();
+      },
+      mounted: function () {
+
       },
       methods: {
         postsShow: function() {
           axios.get(`/posts/${this.$route.params.id}.json`).then(response => {
-            console.log(response.data);
-            this.post = response.data;
+            this.post = response.data
+            this.makeMap();
           })
         },
         postEdit: function () {
@@ -24,6 +28,24 @@
           axios.patch(`http://localhost:3000/posts/${this.post.id}.json`, this.post).then(response => {
           console.log(response.data)
           })
+        },
+        makeMap: function() {
+          const coordinates = document.getElementById('coordinates');
+          mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY
+          const postLng = parseFloat(this.post.longitude)
+          const postLat = parseFloat(this.post.latitude)
+        
+          const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [postLng, postLat], // starting position [lng, lat]
+            zoom: 13, // starting zoom
+          });
+          const marker = new mapboxgl.Marker({
+          draggable: true
+          })
+          .setLngLat([postLng, postLat])
+          .addTo(map);
         }
       },
     };
@@ -40,6 +62,7 @@
 
       <dialog id="edit-post">
       <form method="dialog">
+        <div id='map' style='width: 320px; height: 320px;'></div>
         <p><b>Description:</b> <input type="text" v-model="post.description" /></p>
         <p><b>Image URL:</b> <input type="text" v-model="post.image_url" /></p>
         <p><b>Longitude:</b> <input type="text" v-model="post.longitude" /></p>
