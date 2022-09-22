b7e9bd58896cf3e94b19978ff0ea2da03f0f5e0e
Alex Via <alexandervia@gmail.com>
2022-09-22 14:06:13 -0500

    adds update post function

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -18,8 +18,15 @@
             this.post = response.data;
           })
         },
-        postUpdate: function () {
+        postEdit: function () {
           console.log("updating post")
+          document.querySelector('#edit-post').showModal()
+        },
+        updatePost: function() {
+          console.log('updating product...')
+          axios.patch(`http://localhost:3000/posts/${this.post.id}.json`, this.post).then(response => {
+          console.log(response.data)
+          })
         }
       },
     };
@@ -32,6 +39,19 @@
         <img :src="post.image_url" />
         <p><b>{{ post.description }}</b></p>
       </div>
+      <button v-on:click="postEdit">Edit Post</button>
+
+      <dialog id="edit-post">
+      <form method="dialog">
+        <p><b>Description:</b> <input type="text" v-model="post.description" /></p>
+        <p><b>Image URL:</b> <input type="text" v-model="post.image_url" /></p>
+        <p><b>Longitude:</b> <input type="text" v-model="post.longitude" /></p>
+        <p><b>Latitude:</b> <input type="text" v-model="post.latitude" /></p>
+        <button v-on:click="updatePost()">Update Post</button>
+        <button v-on:click="destroyProduct(currentProduct)">Delete Product</button>
+        <button>Close</button>
+      </form>
+    </dialog>
     </div>
   </template>
   
