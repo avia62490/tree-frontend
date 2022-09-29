982d08589010b8ab4945efd4c636d7b45624bc10
Alex Via <alexandervia@gmail.com>
2022-09-29 13:35:22 -0500

    adds theme to PostsShow page, edits some css options(no effect apparent)

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -1,93 +1,104 @@
 <script>
   import axios from 'axios';
   import mapboxgl from 'mapbox-gl';
-    export default {
-      data: function () {
-        return {
-          message: "This is a POST!",
-          post: {},
-          marker: {}
-        };
-      },
-      created: function () {
-        this.postsShow();
-      },
-      mounted: function () {
+  export default {
+    data: function () {
+      return {
+        message: "This is a POST!",
+        post: {},
+        marker: {}
+      };
+    },
+    created: function () {
+      this.postsShow();
+    },
+    mounted: function () {
 
+    },
+    methods: {
+      postsShow: function() {
+        axios.get(`/posts/${this.$route.params.id}.json`).then(response => {
+          this.post = response.data
+          this.makeMap();
+        })
       },
-      methods: {
-        postsShow: function() {
-          axios.get(`/posts/${this.$route.params.id}.json`).then(response => {
-            this.post = response.data
-            this.makeMap();
-          })
-        },
-        postEdit: function () {
-          document.querySelector('#edit-post').showModal()
-        },
-        updatePost: function() {
-          axios.patch(`http://localhost:3000/posts/${this.post.id}.json`, this.post).then(response => {
-          console.log(response.data)
-          })
-        },
-        makeMap: function() {
-          const coordinates = document.getElementById('coordinates');
-          mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY
-          const postLng = parseFloat(this.post.longitude)
-          const postLat = parseFloat(this.post.latitude)
-        
-          const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [postLng, postLat], // starting position [lng, lat]
-            zoom: 13, // starting zoom
-          });
-          this.marker = new mapboxgl.Marker({
-          draggable: true
-          })
-          .setLngLat([postLng, postLat])
-          .addTo(map);
+      postEdit: function () {
+        document.querySelector('#edit-post').showModal()
+      },
+      updatePost: function() {
+        axios.patch(`http://localhost:3000/posts/${this.post.id}.json`, this.post).then(response => {
+        console.log(response.data)
+        })
+      },
+      makeMap: function() {
+        const coordinates = document.getElementById('coordinates');
+        mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY
+        const postLng = parseFloat(this.post.longitude)
+        const postLat = parseFloat(this.post.latitude)
+      
+        const map = new mapboxgl.Map({
+          container: 'map', // container ID
+          style: 'mapbox://styles/mapbox/light-v10', // style URL
+          center: [postLng, postLat], // starting position [lng, lat]
+          zoom: 13, // starting zoom
+        });
+        this.marker = new mapboxgl.Marker({
+        draggable: true
+        })
+        .setLngLat([postLng, postLat])
+        .addTo(map);
 
-          this.marker.on('dragend', this.onDragEnd);
-        },
-        onDragEnd() {
-          const lngLat = this.marker.getLngLat();
-          console.log(lngLat.lng)
-          console.log(lngLat.lat)
-          this.post.longitude = lngLat.lng
-          this.post.latitude = lngLat.lat
-        }
+        this.marker.on('dragend', this.onDragEnd);
       },
-    };
-  </script>
+      onDragEnd() {
+        const lngLat = this.marker.getLngLat();
+        console.log(lngLat.lng)
+        console.log(lngLat.lat)
+        this.post.longitude = lngLat.lng
+        this.post.latitude = lngLat.lat
+      }
+    },
+  };
+</script>
   
-  <template>
-    <div class="home">
-      <h1>{{ message }}</h1>
-      <div>
+<template>
+  <section class="about-section text-center" id="about">
+    <div class="container px-4 px-lg-5">
+      <div class="col-lg-10">
         <img :src="post.image_url" />
-        <p><b>{{ post.description }}</b></p>
       </div>
-      <button v-on:click="postEdit">Edit Post</button>
-
-      <dialog id="edit-post">
-      <form method="dialog">
-        <div id='map' style='width: 320px; height: 320px;'></div>
-        <p><b>Description:</b> <input type="text" v-model="post.description" /></p>
-        <p><b>Image URL:</b> <input type="text" v-model="post.image_url" /></p>
-        <p><b>Longitude:</b> <input type="text" v-model="post.longitude" /></p>
-        <p><b>Latitude:</b> <input type="text" v-model="post.latitude" /></p>
-        <button v-on:click="updatePost()">Update Post</button>
-        <button v-on:click="destroyProduct(currentProduct)">Delete Product</button>
-        <button>Close</button>
-      </form>
-    </dialog>
+      <br/>
+      <p class="description-text"><b>{{ post.description }}</b></p>
+      <button class="btn btn-primary" v-on:click="postEdit">Edit Post</button>
     </div>
-  </template>
-  
-  <style>
-    img {
-      width: 80%;
-      height: auto;
-    }
-  </style>
+    <br/>
+  </section>
+
+  <dialog id="edit-post">
+    <form method="dialog">
+      <div id='map' class="mapDisplay"></div>
+      <p><b>Description:</b> <input type="text" v-model="post.description" /></p>
+      <p><b>Image URL:</b> <input type="text" v-model="post.image_url" /></p>
+      <p><b>Longitude:</b> <input type="text" v-model="post.longitude" /></p>
+      <p><b>Latitude:</b> <input type="text" v-model="post.latitude" /></p>
+      <button class="btn btn-primary" v-on:click="updatePost()">Update Post</button>
+      <button class="btn btn-light">Close</button>
+      <button class="btn btn-danger" v-on:click="destroyProduct(currentProduct)">Delete Product</button>
+    </form>
+  </dialog>
+
+</template>
+
+<style>
+  .mapDisplay {
+    width: 100%;
+    height: 80%;
+  }
+  img {
+    width: 80%;
+    height: auto;
+  }
+  .description-text {
+    color:#64a19d;
+  }
+</style>
