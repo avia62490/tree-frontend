b66f366c577c298b7ba2cfb9354311ed40892782
Alex Via <alexandervia@gmail.com>
2022-09-29 14:16:23 -0500

    applies them to Signup page

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -25,29 +25,43 @@
 </script>
 
 <template>
-  <div class="signup">
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Name:</label>
-        <input type="text" v-model="newUserParams.name" />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newUserParams.email" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newUserParams.password" />
-      </div>
-      <div>
-        <label>Password confirmation:</label>
-        <input type="password" v-model="newUserParams.password_confirmation" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
-  </div>
+  <section class="signup-section" id="signup">
+    <div class="container px-4 px-lg-5">
+      <form v-on:submit.prevent="submit()">
+        <div class="col-md-2"><h2 class="sign-up-text">Signup</h2></div>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label class="sign-up-text">Name:</label>
+          <input class="form-control" type="text" placeholder="Enter user name..." v-model="newUserParams.name" />
+        </div>
+        <div>
+          <label class="sign-up-text">Email:</label>
+          <input class="form-control" type="email" placeholder="Enter email address..." v-model="newUserParams.email" />
+        </div>
+        <div>
+          <label class="sign-up-text">Password:</label>
+          <input class="form-control" type="password" placeholder="Enter password..." v-model="newUserParams.password" />
+        </div>
+        <div>
+          <label class="sign-up-text">Password confirmation:</label>
+          <input class="form-control" type="password" placeholder="Confirm password..." v-model="newUserParams.password_confirmation" />
+        </div>
+        <br/>
+        <div class="col-md-10 col-lg-8 mx-auto text-center">
+          <input class="btn btn-primary" type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
+
+<style>
+  .sign-up-text {
+    color: white;
+    background-color: #64a19d;
+    align-content: center;
+    align-items: center;
+  }
+</style>
